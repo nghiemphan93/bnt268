c9daf22d9fa6fb0621f3c8a21dce99d059969ca0
Nghiem Phan <nghiemphan93@gmail.com>
2020-04-11 21:14:38 +0700

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    firebase: {
+        apiKey: 'AIzaSyAWkWoKx_cwVCEu0YFn2MkUl5EzvC_k4LY',
+        authDomain: 'bnt268-f7f50.firebaseapp.com',
+        databaseURL: 'https://bnt268-f7f50.firebaseio.com',
+        projectId: 'bnt268-f7f50',
+        storageBucket: 'bnt268-f7f50.appspot.com',
+        messagingSenderId: '490892368072',
+        appId: '1:490892368072:web:c73a6d9caaf0434a3d6dfd',
+        measurementId: 'G-F12VSV5XEG'
+    }
 };
 
 /*
